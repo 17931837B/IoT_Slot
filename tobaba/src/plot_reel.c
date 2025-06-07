03d28380b6a570328d75dbf9198dadd1878ef743
17931837B <tomoya.bb1014@gmail.com>
2025-06-07 14:30:20 +0900

    sisaku No1

--- tobaba/src/plot_reel.c
+++ tobaba/src/plot_reel.c
@@ -1,19 +1,11 @@
 #include "../slot.h"
-#include "../reel.h"
-
-long long current_time;
-long long last_time;
-long long count_time = 0;
-int	now_pos_l = 0;
-int	now_pos_r = 0;
-int	now_pos_s = 0;
 
 int	plot_reel1(t_param *param)
 {
 	int	i;
 	int	j;
 	int	k, l;
-	int	o_x = HOUSING + 15;
+	int	o_x = HOUSING + 10;
 	int	o_y = HOUSING;
 
 	if (param->is_up_left || !reel[(H_REEL / MAG_RATE / 2 - (now_pos_l % REEL_Y) + REEL_Y) % REEL_Y][16])
@@ -61,7 +53,7 @@ int	plot_reel2(t_param *param)
 	int	o_y = HOUSING;
 
 	if (param->is_up_senter || !reel[(H_REEL / MAG_RATE / 2 - (now_pos_s % REEL_Y) + REEL_Y) % REEL_Y][16])
-		now_pos_s = count_time + 10;
+		now_pos_s = count_time;
 	i = 0;
 	while (i < H_REEL / MAG_RATE)
 	{
@@ -101,11 +93,11 @@ int	plot_reel3(t_param *param)
 	int	i;
 	int	j;
 	int	k, l;
-	int	o_x = 660;
+	int	o_x = 640;
 	int	o_y = HOUSING;
 
 	if (param->is_up_right || !reel[(H_REEL / MAG_RATE / 2 - (now_pos_r % REEL_Y) + REEL_Y) % REEL_Y][16])
-		now_pos_r = count_time + 20;
+		now_pos_r = count_time;
 	i = 0;
 	while (i < H_REEL / MAG_RATE)
 	{
@@ -139,52 +131,3 @@ int	plot_reel3(t_param *param)
 	else
 		return (reel[(H_REEL / MAG_RATE / 2 - (now_pos_r % REEL_Y) + REEL_Y) % REEL_Y][16]);
 }
-
-static int	main_loop(t_param *param)
-{
-	int	x;
-	int	y;
-	int	role[3];
-
-	current_time = get_time_in_ns();
-	if (current_time - last_time > ONE_SECONDS / FPS)
-	{
-		y = 0;
-		while (y < HEIGHT)
-		{
-			x = 0;
-			while (x < WIDTH)
-			{
-				if (x < HOUSING || WIDTH - HOUSING < x || y < HOUSING || HEIGHT - HOUSING < y)
-					paint_color(&param->img, x, y, rgb_num(255, 0, 0));
-				else if (y == HEIGHT / 2)
-					paint_color(&param->img, x, y, rgb_num(255, 0, 0));
-				else if (!((WIDTH - 2 * (HOUSING + LINE)) / 3 + HOUSING <= x && x < (WIDTH - 2 * (HOUSING + LINE)) / 3 + HOUSING + LINE) && !((WIDTH - 2 * (HOUSING + LINE)) * 2 / 3 + HOUSING + LINE <= x && x < (WIDTH - 2 * (HOUSING + LINE)) * 2 / 3 + HOUSING + 2 * LINE))
-					paint_color(&param->img, x, y, rgb_num(255, 255, 255));
-				x++;
-			}
-			y++;
-		}
-		role[0] = plot_reel1(param);
-		role[1] = plot_reel2(param);
-		role[2] = plot_reel3(param);
-		printf("%d, %d, %d\n", role[0], role[1], role[2]);
-		mlx_put_image_to_window(param->mlx, param->win, param->img.img, 0, 0);
-		count_time++;
-		last_time = current_time;
-	}
-	return (0);
-}
-
-int main(void)
-{
-	t_param	param;
-
-	create_param(&param);
-	mlx_hook(param.win, KeyPress, KeyPressMask, key_press_hook, &param);
-	mlx_hook(param.win, ClientMessage, StructureNotifyMask, exit_param, &param);
-	mlx_loop_hook(param.mlx, &main_loop, &param);
-	last_time = get_time_in_ns();
-	mlx_loop(param.mlx);
-	return (0);
-}	
